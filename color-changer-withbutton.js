3f39d57bb1a07ba5217c270a5c9c829734136d35
Yavuz Selim Ucar <ysia1912@hotmail.com>
2022-08-04 12:50:20 +0300

    Everything is checked

--- color-changer-withbutton.js
+++ color-changer-withbutton.js
@@ -1,8 +1,7 @@
-var bgColor = document.querySelector("#square")
-function random() {  
-    var array = ["black","red","pink","yellow","green"];
-    var backgroundcolor = array[Math.floor(Math.random()*5)];
-    bgColor.style.backgroundColor=(backgroundcolor)
-    return
+var bgColor = document.querySelector("#square")// this line is for describing to div.
+function random() {  //this line is for describing a function.
+    var array = ["blue","red","pink","yellow","green"];//this line is for create a new array with color.
+    var backgroundcolor = array[Math.floor(Math.random()*5)]; // this line is for picking a random number and rounding that number up.this number is between 1 and 5. array[1<number<5]
+    bgColor.style.backgroundColor=(backgroundcolor)//this line is for change to background.
 }
 
